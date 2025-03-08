c3128bd142ed99fc82b3a3fcc5f70525755f1fa5
iamdurgesh <durgesh.april25@gmail.com>
2025-03-08 18:37:19 +0100

    # javascript quick recap

---
+++ js_prac/index.js
@@ -0,0 +1,47 @@
+let A = "This is a separate file"
+
+
+console.log(A)
+// 1. Javascript Variables (Var, let & const)
+var num1 = 34
+var num2 = 43
+console.log (num1+num2 )
+
+// 2.  Datatypes in javascript Primitive : (number, string, boolean, undefined, null), Reference: (Objects, arrays)
+ // Object
+var obj = {
+    num : 23,
+    string : "String",
+    boolean : true,
+}
+console.log(obj)
+
+// Arrays:
+
+var arr = [1,2,3,4,5,6]
+
+// 3. Operators
+// Arithmatic operators
+var a = 99;
+var b = 9;
+
+// console.log("The value of a + b = ", a + b)
+// console.log("The value of a - b = ", a - b)
+// console.log("The value of a / b = ", a / b)
+// console.log("The value of a * b = ", a * b)
+
+// Assignment operators:
+var c = b;
+// console.log(c)
+// c += 10;
+// c -= 10;
+// c *= 10;
+// c /= 10;
+// console.log(c)
+
+// Comparision operators
+console.log(a == b)
+console.log(c == b)
+
+// Functions & Scope:
+
